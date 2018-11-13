aaf98853b59c636c63005f975998538dfac28da6
pontus göth <pontus.goth@simumatik.com>
2018-11-13 12:45:07 +0100

    added form for booking, added button and nbutton styling

--- src/main.js
+++ src/main.js
@@ -1,24 +1,28 @@
 /*Form validation */
+let loginform = document.getElementById("logindiv");
 let loginbtn = document.getElementById("loginbtn");
 loginbtn.addEventListener("click", loginCheck)
 
 let unameinput = document.getElementById("uname");
 let unamevalue = unameinput.value;
 
-let pswdinput = document.getElementById("pswd");
-let pswdvalue = pswdinput.value;
-
-let regpat = "/[A-Za-z]*\@[a-z]*/";
+/*Book form*/
+let user = document.getElementById("loggedin");
+let bookingform = document.getElementById("bookform");
+bookingform.style.display = "none";
 
 
 function loginCheck(){
     console.log("Called logincheck");
-    if(unamevalue.match(regpat)!=null){
-        unameinput.style.backgroundColor = "#cfa";
-        console.log("failed");
+    if(unamevalue.match(/[A-Za-z0-9.]*\@[a-z]*/)!=null){
+        unameinput.style.backgroundColor = "green";
+        console.log("success email");
+        loginform.style.display ="none";
+        bookingform.style.display = "block";
+        user.innerHTML = "Logged in user: " + unamevalue;
       }else{
-        unameinput.style.backgroundColor = "#f57";
-        console.log("succes");
+        unameinput.style.backgroundColor = "red";
+        console.log("fail email");
       }
 }
 
