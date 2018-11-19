d72dfcb1cb27f9a0d812beae43b40db07720572c
Pontus <pontus.g91@gmail.com>
2018-11-19 11:06:49 +0100

    added sign up page, started working on form validation for booking, added search placeholder, made changes to booking cart

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
+/**
+ * TODO:
+ * Function som hanterar att visa/inte visa "pages"
+ * Do form validation with Jquery for booking form
+ * */
+
+/*Error handeling */
+let error = document.getElementById('error');
+
 /*Login*/
 let loginform = document.getElementById("logindiv");
 let loginbtn = document.getElementById("loginbtn");
 loginbtn.addEventListener("click", check_email);
 
+
 let user = document.getElementById("loggedin");
 
-function check_stored_user(){
-  if(localStorage.getItem('user') != null){
+function check_stored_user() {
+  if (localStorage.getItem('user') != null) {
     loginform.style.display = "none";
     bookingform.style.display = "block";
+    bookings.style.display = 'block';
   }
-  else{
+  else {
     console.log("user not loged in before");
   }
+}
+/*Sign up*/
+let SignUpBtn = document.getElementById('signup');
+document.getElementById('signUpPage').style.display = 'none';
+SignUpBtn.addEventListener('click',sign_up);
+function sign_up(){
+document.getElementById('signUpPage').style.display = 'block';
 }
 
 function check_email() {
   let unameinput = document.getElementById("uname");
   let unamevalue = unameinput.value;
-  let error = document.getElementById('error');
 
   if (unamevalue.match(/[A-Za-z0-9.]+@/) != null) {
     loginform.style.display = "none";
@@ -26,6 +43,7 @@ function check_email() {
     user.innerHTML = "Logged in user: " + remove_char_from_string(unamevalue, '@');
     store_logedin_user(unamevalue);
     Bookings(unamevalue);
+    error.innerHTML = '';
   } else {
     unameinput.style.borderColor = "red";
     error.innerHTML = 'You did not send a valid email, please check and try again :)';
@@ -35,7 +53,7 @@ function check_email() {
 function store_logedin_user(activeUser) {
   if (typeof (Storage) !== "undefined") {
     localStorage.setItem("user", activeUser);
-    console.log("web storage: ",localStorage.getItem('user'));
+    console.log("web storage: ", localStorage.getItem('user'));
   } else {
     alert("Web storage is not supported by your browser! Login will not be saved");
   }
@@ -43,7 +61,7 @@ function store_logedin_user(activeUser) {
 
 /*Logout */
 let logoutBtn = document.getElementById('logoutBtn');
-logoutBtn.addEventListener('click',logout);
+logoutBtn.addEventListener('click', logout);
 
 function logout() {
   loginform.style.display = "block";
@@ -71,19 +89,49 @@ bookingform.style.display = "none";
 let BookWalker = document.getElementById('Book');
 BookWalker.addEventListener('click', Bookwalker);
 
+//Function to be able to book a walker
 function Bookwalker() {
-  let bookedWalker = document.getElementById('walkerName').value;
-  let bookedWalkers = document.getElementById("bookedWalkers");
+  let WalkerName = document.getElementById('walkerName').value;
+  let WalkDistance = document.getElementById('distance').value;
+  let choice_one = document.getElementById('c1');
+  let choice_one_value;
+  let choice_two = document.getElementById('c2');
+  let choice_two_value;
+  let choice_three = document.getElementById('c3');
+  let choice_three_value;
+  let cart = document.getElementById("cart");
 
-  bookedWalkers.innerHTML += "<li>" + bookedWalker + "</li>";
+  if (choice_one.checked) {
+    choice_one_value = choice_one.value;
+    console.log("choice: ", choice_one_value);
+  } else if (choice_two.checked) {
+    choice_two_value = choice_two.value;
+    console.log("choice: ", choice_two_value);
+  } else if (choice_three.checked) {
+    choice_three_value = choice_three.value;
+    console.log("choice: ", choice_three_value);
+  }
+
+  if (WalkerName.value != '' && WalkDistance != '' && choice_one_value != '' || choice_two_value != '' || choice_three_value != '') {
+    cart.innerHTML += "<li>" + WalkerName.value + "</li>";
+    error.innerHTML = '';
+    BookWalker.style.borderBottomColor = "green";
+  } else {
+    //console.log(bookedWalker.value);
+    BookWalker.style.borderColor = "red";
+    error.innerHTML = 'You did not enter correct information to book a walker';
+  }
 }
 
+/*Distance slider */
 let distanceSlide = document.getElementById("distance");
 let distanceOutput = document.getElementById("distanceOutput");
-function slideShowValue(){
+
+function slideShowValue() {
   distanceOutput.value = distanceSlide.value;
 }
 
+/*Function for removing characters from string */
 function remove_char_from_string(text, character) {
   text = text.replace(character, '');
   return text;
