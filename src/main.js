a47d15c0db03623cc1bf715b3b414bbd67a390e6
Pontus <pontus.g91@gmail.com>
2018-11-23 15:43:03 +0100

    Finished user signup with db and ajax, working on user login

--- src/main.js
+++ src/main.js
@@ -4,8 +4,17 @@
  * add keypress event
  * */
 
+
+/**
+ * GLOBAL VARIABLES:
+ *  USERNAME - hold the account name of a user that exists in the database
+ */
+
+ let USERNAME = ''
+
+
 /*Error handeling */
-function errors(err){
+function errors(err) {
   let errorElem = document.getElementById('error');
   //unameinput.style.borderColor = "red";
   errorElem.innerHTML = err;
@@ -17,7 +26,7 @@ Parameters:
 -type: string
 -content: name of content to show
 */
-function content_handler(content){
+function content_handler(content) {
   let loginform = document.getElementById("logindiv");
   let UserPage = document.getElementById("userpage");
   let LogOutBtn = document.getElementById("logoutBtn");
@@ -25,19 +34,19 @@ function content_handler(content){
   //console.log('content: ', content);
 
   LogOutBtn.addEventListener('click', logout);
-  
-  if(content === 'logindiv'){
+
+  if (content === 'logindiv') {
     loginform.style.display = "block";
     UserPage.style.display = "none";
     LogOutBtn.style.display = 'none';
-    Usr.style.display ="none";
-  
-  }else if(content === 'userpage'){
-  document.getElementById('signUpdiv').style.display = 'none';
-  loginform.style.display = "none";
-  UserPage.style.display = "block";
-  LogOutBtn.style.display = 'block';
-  Usr.style.display ="block";
+    Usr.style.display = "none";
+
+  } else if (content === 'userpage') {
+    document.getElementById('signUpdiv').style.display = 'none';
+    loginform.style.display = "none";
+    UserPage.style.display = "block";
+    LogOutBtn.style.display = 'block';
+    Usr.style.display = "block";
   }
 }
 
@@ -53,7 +62,7 @@ function check_stored_user() {
   }
   else {
     content_handler('logindiv');
-    console.log("no user logged in");
+    //console.log("no user logged in");
   }
 }
 
@@ -62,13 +71,20 @@ function check_creds() {
   let unamevalue = unameinput.value;
 
   if (unamevalue.match(/[A-Öa-ö]/) != null) {
-    store_logedin_user(unamevalue);
-    active_user(unamevalue);
-    content_handler('userpage');
+    get_user(unamevalue);
+    console.log("db: ",USERNAME, "form: ", unamevalue);
+    if(USERNAME === unamevalue){
+      console.log('username: ',USERNAME);
+    }
+    //store_logedin_user(unamevalue);
+    //active_user(unamevalue);
+    //content_handler('userpage');
   } else {
-    errors("Username did not match account");
+    errors("Username is wrongly inputed");
   }
 }
+
+
 
 function store_logedin_user(activeUser) {
   if (typeof (Storage) !== "undefined") {
@@ -81,7 +97,6 @@ function store_logedin_user(activeUser) {
 
 /*Logout */
 function logout() {
-
   localStorage.removeItem('user');
   content_handler('logindiv');
 }
@@ -89,7 +104,7 @@ function logout() {
 /*Function for showin a welcome message for active user */
 function active_user(activeUser) {
   let ActiveUser = document.getElementById('activeuser');
-  ActiveUser.innerHTML =  "logedin user: " +activeUser;
+  ActiveUser.innerHTML = "logedin user: " + activeUser;
 }
 
 /*Book form*/
@@ -123,9 +138,9 @@ function Bookwalker() {
     Choice = choice_three_value;
   }
 
-    cart.innerHTML += "<li>" + WalkerName.value + " walk with:" +Choice + " for:"+WalkDistance+"km" + "</li>";
-    error.innerHTML = '';
-    BookWalker.style.borderBottomColor = "green";
+  cart.innerHTML += "<li>" + WalkerName.value + " walk with:" + Choice + " for:" + WalkDistance + "km" + "</li>";
+  error.innerHTML = '';
+  BookWalker.style.borderBottomColor = "green";
 
 }
 
@@ -146,18 +161,18 @@ function remove_char_from_string(text, character) {
 let panel = document.getElementById('panel');
 panel.style.display = 'none';
 let UserIcon = document.getElementById('usr');
-UserIcon.addEventListener("mouseover",Show_panel);
+UserIcon.addEventListener("mouseover", Show_panel);
 UserIcon.addEventListener("mouseout", Hide_panel);
 
 
-function Show_panel(){
+function Show_panel() {
   console.log("hovering");
   let y_pos = 40;
   panel.style.display = 'block';
-  panel.style.top = y_pos+'px';
+  panel.style.top = y_pos + 'px';
 }
 
-function Hide_panel(){
+function Hide_panel() {
   console.log("Not hovering");
   panel.style.display = 'none';
 }
@@ -167,83 +182,105 @@ function Hide_panel(){
 /*Sign up*/
 $('#signUpdiv').hide();
 
-$("#signUpBtn").click(function(e) {
+$("#signUpBtn").click(function (e) {
   $('#signUpdiv').show(1000);
 });
 
-$("#cancel").click(function(e) {
+$("#cancel").click(function (e) {
   $('#signUpdiv').hide(1000);
 });
 
-$("#bday").keyup(function(e){
+$("#bday").keyup(function (e) {
   let EnterKey = e.which;
-  if(EnterKey === 13){
+  if (EnterKey === 13) {
     sign_up_validation();
   }
 });
 
-$("#signup").click(function(e) {
+$("#signup").click(function (e) {
   sign_up_validation();
 });
- 	
+
 
 $("#bday").datepicker();
 
-function sign_up_validation(){
+function sign_up_validation() {
   let Fname = $('#fname').val();
   let Lname = $('#lname').val();
   let Adress = $('#adress').val();
   let Cellphone = $('#cellphonenr').val();
   let Email = $('#email').val();
+  let UserName = $('#usrname').val();
 
 
-    if (Fname.match(/[A-Öa-ö]/) != null) {
-      console.log("fname ok:",Fname);
-    } else {
-      errors("First name was not input correctly");
-    }
-    if (Lname.match(/[A-Öa-ö]/) != null) {
-      console.log("lname ok:",Lname);
-    } else {
-      errors("Last name was not input correctly");
-    }
-    if (Adress.match(/[A-Öa-Ö]+\s+\d.+\d.+[A-Öa-ö]/) != null) {
-      console.log("Adress ok:",Adress);
-    } else {
-      errors("Not a valid adress");
-    }
-    if (Cellphone.match(/[0-9]+.*/) != null) {
-      console.log("Cellphone ok:",Cellphone);
-    } else {
-      errors("Not a correct cellphone number");
-    }
-    if (Email.indexOf("@")==-1) {
-      errors("Missing @");
-    } else {
-      console.log("Email ok",Email)
-    }
-    
-    add_new_usr_to_db(Fname,Lname,Adress,Cellphone,Email);
+  if (UserName.match(/[A-Öa-ö]/) != null) {
+    console.log("username ok:", UserName);
+  } else {
+    errors("First name was not input correctly");
+  }
+  if (Fname.match(/[A-Öa-ö]/) != null) {
+    console.log("fname ok:", Fname);
+  } else {
+    errors("First name was not input correctly");
+  }
+  if (Lname.match(/[A-Öa-ö]/) != null) {
+    console.log("lname ok:", Lname);
+  } else {
+    errors("Last name was not input correctly");
+  }
+  if (Adress.match(/[A-Öa-Ö]+\s+\d.+\d.+[A-Öa-ö]/) != null) {
+    console.log("Adress ok:", Adress);
+  } else {
+    errors("Not a valid adress");
+  }
+  if (Cellphone.match(/[0-9]+.*/) != null) {
+    console.log("Cellphone ok:", Cellphone);
+  } else {
+    errors("Not a correct cellphone number");
+  }
+  if (Email.indexOf("@") == -1) {
+    errors("Missing @");
+  } else {
+    console.log("Email ok", Email)
+  }
+
+  add_new_customer(UserName, Fname, Lname, Adress, Cellphone, Email);
 }
 
-function add_new_usr_to_db(fname,lname,adress,cellphone,email){
-
+function add_new_customer(username, fname, lname, adress, cellphone, email) {
   $.ajax({
     type: 'POST',
     url: 'src/API/booking/makecustomer_XML.php',
     data: {
-      id: 'pg9003',
-      firstname: escape(fname),
-      lastname: escape(lname),
-      email: escape(email),
-      address: escape(adress),
-      auxdata:escape(cellphone)
+      ID: encodeURIComponent(username),
+      firstname: encodeURIComponent(fname),
+      lastname: encodeURIComponent(lname),
+      email: encodeURIComponent(email),
+      address: encodeURIComponent(adress),
+      auxdata: encodeURIComponent(cellphone)
     },
-    success:  returned		
+    success: returned
   });
 }
 
-function returned(returnedData){
-  var resultset=returnedData;
-  console.log(resultset);
+function get_user(uname) {
+  $.ajax({
+    type: 'POST',
+    url: 'src/API/booking/getcustomer_XML.php',
+    data: {
+      customerID: encodeURIComponent(uname),
+    },
+    success: returned_user
+  });
+}
+
+function returned_user(returnedData) {
+  var resultset = returnedData.childNodes[0];
+  for (let i = 0; i < resultset.childNodes.length; i++) {
+    var customer = resultset.childNodes.item(i);
+    if (customer.nodeName === "customer") {
+      USERNAME = customer.attributes['id'].nodeValue;
+      //console.log(USERNAME);
+    }
+  }
 }
